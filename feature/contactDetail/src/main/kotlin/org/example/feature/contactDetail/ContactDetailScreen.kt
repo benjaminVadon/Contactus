eca6c6c5026a4e84299ff4909912f52438efedd1
Benjamin Vadon <benjamin.vadon@majelan.audio>
2024-06-17 00:47:24 +0200

    add transition for detail screen

--- feature/contactDetail/src/main/kotlin/org/example/feature/contactDetail/ContactDetailScreen.kt
+++ feature/contactDetail/src/main/kotlin/org/example/feature/contactDetail/ContactDetailScreen.kt
@@ -1,5 +1,7 @@
 package org.example.feature.contactDetail
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -10,7 +12,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 public data class ContactDetailScreen(val contactId: Int)
 
-public fun NavGraphBuilder.contactDetail(): Unit = composable<ContactDetailScreen> {
+public fun NavGraphBuilder.contactDetail(): Unit = composable<ContactDetailScreen>(
+    enterTransition = { slideInHorizontally(initialOffsetX = { it / 2 }) },
+    exitTransition = { slideOutHorizontally(targetOffsetX = { it }) },
+) {
     val contactDetailViewModel: ContactDetailViewModel = hiltViewModel()
     val state by contactDetailViewModel.uiStateFlow.collectAsState()
 
