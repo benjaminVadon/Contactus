69e3707a05431c11b3e87838c27445a0073d51cd
Benjamin Vadon <benjamin.vadon@majelan.audio>
2024-06-17 00:43:04 +0200

    fix icon color

--- feature/contactDetail/src/main/kotlin/org/example/feature/contactDetail/components/Contact.kt
+++ feature/contactDetail/src/main/kotlin/org/example/feature/contactDetail/components/Contact.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +30,9 @@ internal fun Contact(icon: ImageVector, @StringRes name: Int, value: String) {
     ) {
         Image(
             modifier = Modifier.size(30.dp),
-            imageVector = icon, contentDescription = stringResource(id = name)
+            imageVector = icon,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+            contentDescription = stringResource(id = name)
         )
         Text(
             text = value,
